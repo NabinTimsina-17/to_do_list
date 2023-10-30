7c1db7e5768476268f9f0b340d5a23875192e248
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-30 22:42:43 +0545

    catagory card updated

--- lib/reusableWidgets/catagory_card.dart
+++ lib/reusableWidgets/catagory_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/pages/details_page.dart';
 
 class CatagoryCard extends StatelessWidget {
   const CatagoryCard({
@@ -10,53 +11,58 @@ final String catagory;
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      borderRadius: BorderRadius.circular(20),
-      elevation: 15,
-      child: SizedBox(
-        height: 180,
-        width: 180,
-        child: Row(
-          children: [
-            const SizedBox(
-              width: 7,
-            ),
-            Container(
-              height: 140,
-              width: 6,
-              color: Colors.blue,
-            ),
-            const SizedBox(
-              width: 20,
-            ),
-             Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const SizedBox(
-                  height: 18,
-                ),
-                const Icon(
-                  Icons.event_note,
-                  size: 42,
-                ),
-                Text(
-                  "$noOfTask Tasks",
-                  style:
-                    const  TextStyle(color: Colors.grey, fontSize: 18),
-                ),
-                 Text(
-                  catagory,
-                  style:const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.5),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-              ],
-            )
-          ],
+    return InkWell(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const DetailsPage(),),);
+      },
+      child: Material(
+        borderRadius: BorderRadius.circular(20),
+        elevation: 15,
+        child: SizedBox(
+          height: 180,
+          width: 180,
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 7,
+              ),
+              Container(
+                height: 140,
+                width: 6,
+                color: Colors.blue,
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  const Icon(
+                    Icons.event_note,
+                    size: 42,
+                  ),
+                  Text(
+                    "$noOfTask Tasks",
+                    style:
+                      const  TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                   Text(
+                    catagory,
+                    style:const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.5),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

885409e80666cdfb280ec7c4fac1adf553862ae6
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-11-01 09:08:03 +0545

    add item page updated

--- lib/pages/add_item_page.dart
+++ lib/pages/add_item_page.dart
@@ -1,21 +1,16 @@
-
 import 'package:flutter/material.dart';
-
 
 class AddItemsPage extends StatefulWidget {
   const AddItemsPage({super.key});
 
-
-
   @override
- State<AddItemsPage> createState() =>  _AddItemsPageState();
+  State<AddItemsPage> createState() => _AddItemsPageState();
 }
 
- 
 class _AddItemsPageState extends State<AddItemsPage> {
   String selectedCategory = "finance";
 
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(236, 255, 255, 255),
@@ -36,27 +31,119 @@ class _AddItemsPageState extends State<AddItemsPage> {
           ),
         ),
         title: const Padding(
-          padding: EdgeInsets.fromLTRB(0.0, 38.0, 0, 0),
+          padding: EdgeInsets.fromLTRB(120.0, 38.0, 0, 0),
           child: Text(
-            "NewTask",
+            "New Task",
             style: TextStyle(
               fontSize: 25.0,
             ),
           ),
         ),
       ),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: 
-              EdgeInsets.symmetric(horizontal: 30.0, vertical: 16),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 70,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25.0, 15.0, 15.0, 0.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                hintText: 'What are you planning?',
               ),
-          ],
-        ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.fromLTRB(40.0, 0.0, 0.0, 0.0),
+            tileColor: const Color.fromARGB(255, 255, 255, 255),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            leading: const Icon(
+              Icons.radio_button_unchecked,
+              color: Color.fromARGB(255, 23, 142, 93),
+            ),
+            title: const Text(
+              "Work",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.fromLTRB(40.0, 0.0, 0.0, 0.0),
+            tileColor: const Color.fromARGB(255, 255, 255, 255),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            leading: const Icon(
+              Icons.radio_button_unchecked,
+              color: Color.fromARGB(255, 32, 24, 193),
+            ),
+            title: const Text(
+              "Finance",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.fromLTRB(40.0, 0.0, 0.0, 0.0),
+            tileColor: const Color.fromARGB(255, 255, 255, 255),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            leading: const Icon(
+              Icons.radio_button_unchecked,
+              color: Color.fromARGB(255, 127, 39, 215),
+            ),
+            title: const Text(
+              "Personal",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.fromLTRB(40.0, 0.0, 0.0, 0.0),
+            tileColor: const Color.fromARGB(255, 255, 255, 255),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            leading: const Icon(
+              Icons.radio_button_unchecked,
+              color: Color.fromARGB(255, 20, 20, 202),
+            ),
+            title: const Text(
+              "Study",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => const AddItemsPage())), 
+            child:  const Text('Add Task', ),
+            
+           
+            ), 
+            
+          
+        ],
       ),
     );
   }
